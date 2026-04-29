015e56f45fb1bb0ab3a73b0d5dcf341e1f0c66dd
Manthan-porg <manthansingh5555@gmail.com>
2026-04-29 12:55:11 +0000

    made cart interactive & updated

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -45,7 +45,8 @@ products.forEach((product) => {
           Added
         </div>
 
-        <button class="add-to-cart-button button-primary">
+        <button class="add-to-cart-button button-primary js-add-to-cart-btn"
+        data-product-id="${product.id}">
           Add to Cart
         </button>
       </div>
@@ -54,3 +55,36 @@ products.forEach((product) => {
 })
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+
+document.querySelectorAll('.js-add-to-cart-btn').forEach((button) => {
+    button.addEventListener('click', () => {
+        const productId = button.dataset.productId;
+        let matchingItem;
+        cart.forEach((item) => {
+            if (productId === item.productId) {
+                matchingItem = item;
+            }
+        });
+
+        if (matchingItem) {
+            matchingItem.quantity += 1;
+        } else {
+
+
+            cart.push(
+                {
+                    productId,
+                    quantity: 1,
+                }
+            );
+        }
+
+        let cartQuantity = 0;
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        });
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    });
+
+});
