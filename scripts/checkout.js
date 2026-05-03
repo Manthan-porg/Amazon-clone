642af1e0c9c0f3e6dcaa570b8cbaa9f0498e83dc
Manthan-porg <manthansingh5555@gmail.com>
2026-05-03 20:30:25 +0000

    Fixed price formatting and radio selector

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,32 +1,33 @@
 import {cart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import {formateCurrency} from './utils/money.js';
 
 let orderSummary = ``;
 
 cart.forEach((cartItem)=>{
 
-const  productId = cartItem.productId;
-     
+    const  productId = cartItem.productId;
+
 
     let matchingproduct;
 
-products.forEach((product)=>{
-    
+    products.forEach((product)=>{
 
-if(product.id === productId){
 
-matchingproduct = product;
+        if(product.id === productId){
 
-    console.log(matchingproduct);
-}
-});
+            matchingproduct = product;
+
+            console.log(matchingproduct);
+        }
+    });
 
 
     orderSummary += `
 
-<div class="cart-item-container">
+         <div class="cart-item-container">
             <div class="delivery-date">
-              Delivery date: Tuesday, June 21
+              delivery date: tuesday, june 21
             </div>
 
             <div class="cart-item-details-grid">
@@ -38,67 +39,67 @@ matchingproduct = product;
                   ${matchingproduct.name}
                 </div>
                 <div class="product-price">
-                  $${matchingproduct.priceCents/100}
+                  $${formateCurrency(matchingproduct.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
-                    Quantity: <span class="quantity-label">2</span>
+                    quantity: <span class="quantity-label">2</span>
                   </span>
                   <span class="update-quantity-link link-primary">
-                    Update
+                    update
                   </span>
                   <span class="delete-quantity-link link-primary">
-                    Delete
+                    delete
                   </span>
                 </div>
               </div>
 
               <div class="delivery-options">
                 <div class="delivery-options-title">
-                  Choose a delivery option:
+                  choose a delivery option:
                 </div>
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingproduct.id}">
                   <div>
                     <div class="delivery-option-date">
-                      Tuesday, June 21
+                      tuesday, june 21
                     </div>
                     <div class="delivery-option-price">
-                      FREE Shipping
+                      free shipping
                     </div>
                   </div>
                 </div>
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingproduct.id}">
                   <div>
                     <div class="delivery-option-date">
-                      Wednesday, June 15
+                      wednesday, june 15
                     </div>
                     <div class="delivery-option-price">
-                      $4.99 - Shipping
+                      $4.99 - shipping
                     </div>
                   </div>
                 </div>
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingproduct.id}">
                   <div>
                     <div class="delivery-option-date">
-                      Monday, June 13
+                      monday, june 13
                     </div>
                     <div class="delivery-option-price">
-                      $9.99 - Shipping
+                      $9.99 - shipping
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+         </div>
 
     `
 
