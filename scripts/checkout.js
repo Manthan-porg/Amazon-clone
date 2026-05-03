a205080d4c76ed6133db44ff64452362ddf7af0f
Manthan-porg <manthansingh5555@gmail.com>
2026-05-03 21:19:58 +0000

    removing cart item by clicking delete in DOM

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,14 +18,14 @@ cart.forEach((cartItem)=>{
 
             matchingproduct = product;
 
-            console.log(matchingproduct);
         }
     });
 
 
     orderSummary += `
 
-         <div class="cart-item-container">
+         <div class="cart-item-container 
+         js-cart-item-container-${matchingproduct.id}">
             <div class="delivery-date">
               delivery date: tuesday, june 21
             </div>
@@ -113,6 +113,7 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
 link.addEventListener('click',()=>{
     const productId = link.dataset.productId;
    dltFromCart(productId); 
-    console.log(cart);
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
 });
 });
